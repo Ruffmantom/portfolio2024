eb00d710075070f57c821c5db0585b174614b1a1
Tom Ruff <tomruffcodes@gmail.com>
2024-06-05 08:04:12 -0600

    tried

--- frontend/src/components/underConstructionBanner/UnderConstructionBanner.jsx
+++ frontend/src/components/underConstructionBanner/UnderConstructionBanner.jsx
@@ -4,10 +4,31 @@ import "./style.css";
 const UnderConstructionBanner = () => {
   return (
     <div className="over_screen_view_cont under_construction">
-      <div className="under_contruction_view">
-        <p className="under_construction_text">
-          This website is under construction.
-        </p>
+      <div className="scroll under_contruction_view" style={{ '--time': '20s' }}>
+        <div className="scroll-content">
+          <span>HTML</span>
+          <span>CSS</span>
+          <span>JavaScript</span>
+          <span>ReactJS</span>
+          <span>Figma</span>
+          <span>Photoshop</span>
+          <span>Premiere Pro</span>
+          <span>Figma</span>
+          <span>Angular</span>
+          <span>Node JS</span>
+        </div>
+        <div className="scroll-content">
+          <span>HTML</span>
+          <span>CSS</span>
+          <span>JavaScript</span>
+          <span>ReactJS</span>
+          <span>Figma</span>
+          <span>Photoshop</span>
+          <span>Premiere Pro</span>
+          <span>Figma</span>
+          <span>Angular</span>
+          <span>Node JS</span>
+        </div>
       </div>
     </div>
   );

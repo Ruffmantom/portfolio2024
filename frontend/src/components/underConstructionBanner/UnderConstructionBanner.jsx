b37ee6c5941cc17651897c1321b93738f049128a
Tom Ruff <ruffmantom@gmail.com>
2024-06-05 08:32:49 -0600

    added the under construction banner

--- frontend/src/components/underConstructionBanner/UnderConstructionBanner.jsx
+++ frontend/src/components/underConstructionBanner/UnderConstructionBanner.jsx
@@ -3,31 +3,16 @@ import "./style.css";
 
 const UnderConstructionBanner = () => {
   return (
-    <div className="over_screen_view_cont under_construction">
-      <div className="scroll under_contruction_view" style={{ '--time': '20s' }}>
-        <div className="scroll-content">
-          <span>HTML</span>
-          <span>CSS</span>
-          <span>JavaScript</span>
-          <span>ReactJS</span>
-          <span>Figma</span>
-          <span>Photoshop</span>
-          <span>Premiere Pro</span>
-          <span>Figma</span>
-          <span>Angular</span>
-          <span>Node JS</span>
-        </div>
-        <div className="scroll-content">
-          <span>HTML</span>
-          <span>CSS</span>
-          <span>JavaScript</span>
-          <span>ReactJS</span>
-          <span>Figma</span>
-          <span>Photoshop</span>
-          <span>Premiere Pro</span>
-          <span>Figma</span>
-          <span>Angular</span>
-          <span>Node JS</span>
+    <div class="under_construction_cont">
+      <div class=" carousel-wrapper">
+        <div class="carousel">
+          <div class="item">Under Construction</div>
+          <div class="item">Under Construction</div>
+          <div class="item">Under Construction</div>
+          <div class="item">Under Construction</div>
+          <div class="item">Under Construction</div>
+          <div class="item">Under Construction</div>
+          <div class="item">Under Construction</div>
         </div>
       </div>
     </div>
